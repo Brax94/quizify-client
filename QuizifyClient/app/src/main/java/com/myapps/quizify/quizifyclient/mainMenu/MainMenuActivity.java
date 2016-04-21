0d3c12f5447a94ca11d113d7b75aa1b2c79768df
Elias Bragstad Hagen <elias.b.hagen@gmail.com>
2016-04-21 22:14:28 +0200

    Adds refreshbutton to main menu

--- QuizifyClient/app/src/main/java/com/myapps/quizify/quizifyclient/mainMenu/MainMenuActivity.java
+++ QuizifyClient/app/src/main/java/com/myapps/quizify/quizifyclient/mainMenu/MainMenuActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -66,8 +67,19 @@ public class MainMenuActivity extends Activity {
                 finish();
             }
         });
-
+        Button refresh = (Button) findViewById(R.id.refresh);
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                invites.clear();
+                theirTurn.clear();
+                yourTurn.clear();
+                pending.clear();
+                getGames();
+            }
+        });
     }
+
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
